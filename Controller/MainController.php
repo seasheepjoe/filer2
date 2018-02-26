7421e8be57c361c1a3498793d9240e73d4544891
Louis Loiseau-Billon <louis.loiseau-billon@supinternet.fr>
2018-02-26 18:10:23 +0100

    started register form validation

--- Controller/MainController.php
+++ Controller/MainController.php
@@ -13,6 +13,44 @@ class MainController extends BaseController
     {
         $firstname = ' ';
         $lastname = ' ';
+        $email = ' ';
+        $country = ' ';
+        $city = ' ';
+        $birthdate = ' ';
+        $password = ' ';
+        $password_check = ' ';
+        if (isset($_POST['firstname']) && isset($_POST['lastname']) && isset($_POST['email']) && isset($_POST['email'])
+         && isset($_POST['country']) && isset($_POST['city']) && isset($_POST['birthdate']) && isset($_POST['password']) && isset($_POST['password-check'])) {
+             $firstname = htmlentities($_POST['firstname']);
+             $lastname = htmlentities($_POST['lastname']);
+             $email = htmlentities($_POST['email']);
+             $country = htmlentities($_POST['country']);
+             $city = htmlentities($_POST['city']);
+             $birthdate = htmlentities($_POST['birthdate']);
+             $password = $_POST['password'];
+             $password_check = $_POST['password-check'];
+             $link = mysqli_connect('localhost', 'root', '', 'filer2');
+             $q = "INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `country`, `city`, `birthdate`, `password`) VALUES (NULL, '" . $firstname . "', '" . $lastname . "',
+              '" . $email . "', '" . $country . "', '" . $city . "', '" . $birthdate . "', '" . $password . "')";
+            $errors = [];
+
+            if (strlen($firstname) <= 2 || strlen($firstname) > 25) {
+                $errors[] = 'Firstname must be 3-25 long';
+            }
+
+            if (strlen($lastname) <= 2 || strlen($lastname) > 25) {
+                $errors[] = 'Lastname must be 3-25 long';
+            }
+
+            if (!empty($errors)) {
+                foreach($errors as $lol) {
+                    echo $lol;
+                }
+            }else {
+                echo 'No errors';
+            }
+            
+         }
         return $this->render('register.html.twig');
     }
 }
