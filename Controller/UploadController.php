ef53dfad549242f948373d50a51a6ed61666a14e
Louis Loiseau-Billon <louis.loiseau-billon@supinternet.fr>
2018-02-27 01:01:41 +0100

    stocked file data in var

--- Controller/UploadController.php
+++ Controller/UploadController.php
@@ -11,7 +11,8 @@ class UploadController extends BaseController
             $type = $_FILES['file-upload']['type'];
             $size = $_FILES['file-upload']['size'];
             $tmp_name = $_FILES['file-upload']['tmp_name'];
-            
+            $file_data = ['name' => $name, 'type' => $type, 'size' => $size, 'tmp_name' => $tmp_name    ];
+            var_dump($file_data);
         }
         
         return $this->render('upload.html.twig');
